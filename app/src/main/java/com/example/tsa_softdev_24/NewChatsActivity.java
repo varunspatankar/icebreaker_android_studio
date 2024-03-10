e60d49be9c05e1cdcd635de2019f13d6b2b5f1e6
StudentUser101 <>
2024-03-10 15:37:40 -0700

    New Chats button works

--- app/src/main/java/com/example/tsa_softdev_24/NewChatsActivity.java
+++ app/src/main/java/com/example/tsa_softdev_24/NewChatsActivity.java
@@ -1,24 +1,43 @@
 package com.example.tsa_softdev_24;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class NewChatsActivity extends AppCompatActivity {
-
+    public CardView answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_chats);
+
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+         */
+
+        // Find the CardView by its ID
+        answer = (CardView) findViewById(R.id.ans);
+
+        // Set an OnClickListener for Lesson1
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the new activity
+                Intent intent = new Intent(NewChatsActivity.this, OldChats2Activity.class);
+                startActivity(intent);
+            }
         });
     }
 }
