3803714f435857bdd1cf950543e9c358c9c20604
karunyapenumalla <karunya.penumalla@gmail.com>
2024-03-12 20:54:23 -0700

    The Program is connecting to my api and is sending a response

--- app/src/main/java/com/example/tsa_softdev_24/ToneTest.java
+++ app/src/main/java/com/example/tsa_softdev_24/ToneTest.java
@@ -66,13 +66,15 @@ public class ToneTest extends AppCompatActivity implements View.OnClickListener 
 
         if (clickedButton.getId() == R.id.submit_btn) {
             currentQuestionIndex++;
-            loadNewQuestion();
+
 
             if (!selectedAnswer.isEmpty()) {
                 ansA.setBackgroundColor(Color.LTGRAY);
                 ansB.setBackgroundColor(Color.LTGRAY);
                 ansC.setBackgroundColor(Color.LTGRAY);
                 ansD.setBackgroundColor(Color.LTGRAY);
+                score += evaluateTone(selectedAnswer);
+                loadNewQuestion();
             } else {
                 // Show an alert indicating that the user needs to select an option
                 new AlertDialog.Builder(this)
@@ -82,7 +84,7 @@ public class ToneTest extends AppCompatActivity implements View.OnClickListener 
                         .show();
 
                 // Evaluate tone after the user selects an option
-                score += evaluateTone(selectedAnswer);
+
             }
         } else {
             // Choices button clicked
@@ -111,46 +113,46 @@ public class ToneTest extends AppCompatActivity implements View.OnClickListener 
     }
 
 
-    String displayTone(int score) {
+    String displayTone(int avg_Score) {
         // Evaluate the tone based on the selected answers
-        // Determine the tone label based on the tone score
+        // Determine the tone label based on the tone avg_score
         String tonelabel = "";
-        if (score >= 16) {
+        if (avg_Score >= 16) {
             tonelabel = "Positive Tone";
-        } else if (score >= 13) {
+        } else if (avg_Score >= 13) {
             tonelabel = "Optimistic Tone";
-        } else if (score >= 12) {
+        } else if (avg_Score >= 12) {
             tonelabel = "Frustrated Tone";
-        } else if (score >= 11) {
+        } else if (avg_Score >= 11) {
             tonelabel = "Active Tone";
-        } else if (score >= 10) {
+        } else if (avg_Score >= 10) {
             tonelabel = "Ambivalent Tone";
-        } else if (score >= 9) {
+        } else if (avg_Score >= 9) {
             tonelabel = "Neutral Tone";
-        } else if (score >= 8) {
+        } else if (avg_Score >= 8) {
             tonelabel = "Indifferent Tone";
-        } else if (score >= 7) {
+        } else if (avg_Score >= 7) {
             tonelabel = "Calm Tone";
-        } else if (score >= 6) {
+        } else if (avg_Score >= 6) {
             tonelabel = "Reserved Tone";
-        } else if (score >= 5) {
+        } else if (avg_Score >= 5) {
             tonelabel = "Reflective Tone";
-        } else if (score >= 4) {
+        } else if (avg_Score >= 4) {
             tonelabel = "Negative Tone";
-        } else if (score >= 3) {
+        } else if (avg_Score >= 3) {
             tonelabel = "Shy Tone";
-        } else if (score >= 2) {
+        } else if (avg_Score >= 2) {
             tonelabel = "Anxious Tone";
-        } else if (score >= 1) {
+        } else if (avg_Score >= 1) {
             tonelabel = "Introverted Tone";
+        } else {tonelabel = String.valueOf(score);
         }
-        return toneLabel;
+        return tonelabel;
     }
 
     void finishQuiz() {
-        displayTone(evaluateTone(selectedanswers));
         float avg_score = score/5;
-        displayTone(Math.round(avg_score));
+        toneLabel = displayTone(Math.round(avg_score));
         // Show the tone score and label in the completion dialog
         String passStatus = "COMPLETED! Your tone: " + toneLabel;
 
@@ -162,112 +164,80 @@ public class ToneTest extends AppCompatActivity implements View.OnClickListener 
     }
 
 
+    // Evaluate the tone based on the selected answers
     int evaluateTone(String selectedAnswer) {
-        int questionScore = 0;
-        switch (currentQuestionIndex) {
-            case 0: // How was your day?
+        int ans_Score = 0;
+
                 switch (selectedAnswer) {
                     case "My day was great! I accomplished a lot of tasks and enjoyed some downtime as well.":
-                        questionScore = 32;
+                        ans_Score += 16;
                         break;
                     case "It was a tough day. Nothing seemed to go right.":
-                        questionScore = 8;
+                        ans_Score += 4;
                         break;
                     case "It was just another day, nothing special.":
-                        questionScore = 18;
+                        ans_Score += 9;
                         break;
                     case "I’d rather not talk about it.":
-                        questionScore = 12;
+                        ans_Score += 6;
                         break;
-                    default:
-                        break;
-                }
-                break;
 
-            case 1: // What’s your favorite way to spend a weekend?
-                switch (selectedAnswer) {
                     case "I love exploring the outdoors! Hiking, biking, you name it!":
-                        questionScore = 36;
+                        ans_Score += 18;
                         break;
                     case "Doesn’t really matter to me. Weekends are just like any other day.":
-                        questionScore = 16;
+                        ans_Score += 8;
                         break;
                     case "I enjoy spending time with my friends and family.":
-                        questionScore = 2;
+                        ans_Score += 1;
                         break;
                     case "I prefer to have some quiet time alone.":
-                        questionScore = 26;
+                        ans_Score += 13;
                         break;
-                    default:
-                        break;
-                }
-                break;
 
-            case 2: // How do you handle stress?
-                switch (selectedAnswer) {
                     case "I try to stay positive and look at the bright side of things.":
-                        questionScore = 32;
+                        ans_Score += 16;
                         break;
                     case "I tend to get overwhelmed and anxious.":
-                        questionScore = 8;
+                        ans_Score += 4;
                         break;
                     case "I usually go for a run or do some physical activity.":
-                        questionScore = 18;
+                        ans_Score += 9;
                         break;
                     case "I don’t usually get stressed.":
-                        questionScore = 12;
+                        ans_Score += 6;
                         break;
-                    default:
-                        break;
-                }
-                break;
 
-            case 3: // What’s your approach to problem-solving?
-                switch (selectedAnswer) {
                     case "I tackle it head-on and try to find a solution as quickly as possible.":
-                        questionScore = 36;
+                        ans_Score += 18;
                         break;
                     case "I take a step back and try to look at the bigger picture.":
-                        questionScore = 16;
+                        ans_Score += 8;
                         break;
                     case "I get frustrated and sometimes avoid dealing with it.":
-                        questionScore = 2;
+                        ans_Score += 1;
                         break;
                     case "I ask others for help or advice.":
-                        questionScore = 26;
+                        ans_Score += 13;
                         break;
-                    default:
-                        break;
-                }
-                break;
 
-            case 4: // How do you feel about meeting new people?
-                switch (selectedAnswer) {
                     case "I love it! It’s always exciting to meet new people.":
-                        questionScore = 32;
+                        ans_Score += 16;
                         break;
                     case "I’m a bit shy and reserved, so it can be challenging.":
-                        questionScore = 8;
+                        ans_Score += 4;
                         break;
                     case "It depends on the situation and the people.":
-                        questionScore = 18;
+                        ans_Score += 9;
                         break;
                     case "I prefer to stick with my close circle of friends.":
-                        questionScore = 12;
+                        ans_Score += 6;
                         break;
                     default:
                         break;
-                }
-                break;
 
-            default:
-                break;
-        }
-
-        score += questionScore; // Update the overall score
-        return questionScore;
-    }
-
+        }return ans_Score;}
+    // Average the total score
 
 
 
